0505aba81e523ca36616d2dcdec560ed6114d6a8
Aakash <aakash2506c@gmail.com>
2024-05-23 13:16:50 +0530

    About us

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='px-[5%] text-[5vh] text-[#352B53] pt-[3%] '>
-      <h1 className='text-center text-xl font-semibold'>About Us</h1>
-      <p className='mt-[5%] text-[3vh]'>At Quizzo having a legal name as Arvesi Technologies pvt ltd, we're passionate about transforming the learning experience for students. Recognizing the need for an interactive and engaging approach to education, we embarked on a journey to create a platform that goes beyond traditional methods. We specialize in providing an innovative online quiz platform tailored to your school curriculum. Our quizzes are designed to make learning fun, interactive, and personalized. Whether you're prepping for exams, reviewing course material, or just curious to test your knowledge,Quizzo has got you covered</p>
+    <div className='px-[5%]  text-[#352B53] pt-[3%] '>
+      <h1 className='text-center text-[5vh] font-semibold'>About Us</h1>
+      <p className='mt-[5%] text-[2.5vh]'>At Quizzo having a legal name as Arvesi Technologies pvt ltd, we're passionate about transforming the learning experience for students. Recognizing the need for an interactive and engaging approach to education, we embarked on a journey to create a platform that goes beyond traditional methods. We specialize in providing an innovative online quiz platform tailored to your school curriculum. Our quizzes are designed to make learning fun, interactive, and personalized. Whether you're prepping for exams, reviewing course material, or just curious to test your knowledge,Quizzo has got you covered</p>
     </div>
   )
 }
